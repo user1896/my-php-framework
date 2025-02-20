a52e6ba8c0d84a1a2411981762840b08a5422b4e
user1896 <abde.user15@gmail.com>
2025-02-20 23:08:25 +0100

    associative arrays and foreach loop

--- index.php
+++ index.php
@@ -4,18 +4,38 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Demo</title>
+	<style>
+		body{
+			place-items: center;
+			display: grid;
+			height: 100vh;
+			font-family: sans-serif;
+			margin: 0;
+		}
+	</style>
 </head>
 <body>
-	<h1>Hello world</h1>
-	<p>I wanna see it the browser</p>
+
 	<?php
-		$i = 4;
-		while($i>0){
-			?>
-			<p>repeat me</p>
-		<?php
-			$i = $i-1;
-		}
+
+		$people = [
+			[
+				"name" => "Adel",
+				"sport" => "Bodybuilding"
+			],[
+				"name" => "Abderazak",
+				"sport" => "Strongman"
+			]
+			];
 	?>
+
+	<h1>Hellow I'm Nadir</h1>
+	<ul>
+		<?php foreach($people as $person ) : ?>
+			<li>
+				<?= $person['name'] ?> : <?= $person['sport'] ?>
+			</li>
+		<?php endforeach; ?>
+	</ul>
 </body>
 </html>
