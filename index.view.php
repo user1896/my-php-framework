b1668309b895f5f9ea22c0d49ce82ac5031f7935
user1896 <abde.user15@gmail.com>
2025-02-24 20:42:10 +0100

    separate the css from the view

--- index.view.php
+++ index.view.php
@@ -4,15 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Demo</title>
-	<style>
-		body{
-			place-items: center;
-			display: grid;
-			height: 100vh;
-			font-family: sans-serif;
-			margin: 0;
-		}
-	</style>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 
