38d8a3748d591c90b29a9f1a223ecfeced8ce196
user1896 <abde.user15@gmail.com>
2025-02-24 17:33:49 +0100

    implementing our own array_filter function

--- index.php
+++ index.php
@@ -23,17 +23,41 @@
 				"name" => "Adel",
 				"sport" => "Bodybuilding"
 			],[
+				"name" => "Adel",
+				"sport" => "basketball"
+			],[
 				"name" => "Abderazak",
 				"sport" => "Strongman"
+			],[
+				"name" => "Ronie",
+				"sport" => "Power building"
 			]
-			];
+		];
+		
+		function filter($array, $callback){
+			$filtered_array = [];
+
+			foreach($array as $item){
+				if($callback($item)){
+					$filtered_array[] = $item;
+				}
+			}
+
+			return $filtered_array;
+		}
+
+		$filtered_people = filter($people, function($array){
+			return $array["name"] === "Adel";
+		});
+		// This function works the same way the the built-in function in PHP called "array_filter()"
 	?>
 
 	<h1>Hellow I'm Nadir</h1>
+
 	<ul>
-		<?php foreach($people as $person ) : ?>
+		<?php foreach($filtered_people as $person ) : ?>
 			<li>
-				<?= $person['name'] ?> : <?= $person['sport'] ?>
+				<?= $person["name"] ?> : <?= $person["sport"] ?>
 			</li>
 		<?php endforeach; ?>
 	</ul>
