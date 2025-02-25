aafc0d8348acd124f9bb1f30c1984d3de52d5c5d
user1896 <abde.user15@gmail.com>
2025-02-25 17:36:15 +0100

    create the about page

--- about.view.php
+++ about.view.php
@@ -3,12 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Demo</title>
-	<link rel="stylesheet" href="style.css">
+	<title>Document</title>
 </head>
 <body>
-
-	<h1>Hello I'm Nadir</h1>
-
+	
 </body>
 </html>
