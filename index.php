c1253994749644a24030265e1104a28f51eaa69a
user1896 <abde.user15@gmail.com>
2025-03-31 09:26:34 +0200

    Super variables and how to debbug a php file

--- index.php
+++ index.php
@@ -2,8 +2,23 @@
 
 $title = 'Home';
 
+
+function mydebug($var){
+	echo "<pre>";
+	var_dump($var);
+	echo "</pre>";
+
+	die();
+}
+
+// mydebug($_SERVER["REQUEST_URI"]);
+
+/*
+$_SERVER is a super global variable that contains information created in the server, it is an array,
+and if we do "var_dump($_SERVER);" we can see all of its properties.
+for example: $_SERVER["REQUEST_URI"] returns which page we're curently in.
+*/
+
 require "views/index.view.php";
 // A partial is a partial peace of HTML.
 
-// Any variable that we define in our controler will be accesable in the coresponding views and partials.
-// because when we "require" the view page here, it's like we wrote it here directly.
--- views/partials/main.php
+++ views/partials/main.php
@@ -1,1 +1,4 @@
-<h1><?= $title ?> page</h1>
+<!-- add the class="highlight-txt" to h1 only when we're in the home page  -->
+<h1 <?php if($_SERVER["REQUEST_URI"] == '/my-php-framework/') echo ' class="highlight-txt"; ' ?> >
+	<?= $title ?> page
+</h1>
