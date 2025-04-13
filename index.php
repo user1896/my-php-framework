5a11b33166141ebde1a6cc20997e3be01ab88e07
user1896 <abde.user15@gmail.com>
2025-04-13 20:50:15 +0200

    refactore the routing system to use an associative array instead of the if else statement

--- index.php
+++ index.php
@@ -1,42 +1,21 @@
 <?php
 
 require 'functions.php';
-/*
-	PHP offers a powerful function called parse_url() that takes a URL as input and returns an associative 
-	array containing the parsed components. ex:
-*/
-$url = "https://www.youtube.com:8080/feed/subscriptions?param1=value1&param2=value2#section2";
-$my_parsed_url = parse_url($url);
-// mydebug($my_parsed_url);
-/*
-	results:
-	array(6) {
-		["scheme"]=> string(5) "https"
-		["host"]=> string(15) "www.youtube.com"
-		["port"]=> int(8080)
-		["path"]=> string(19) "/feed/subscriptions"
-		["query"]=> string(27) "param1=value1¶m2=value2"
-		["fragment"]=> string(8) "section2"
-	}
-	
-	Now When we try to create a router we use ["path"] to determine the path, so we don't have to worry about the host
-	if it's a localhost or a server provider, and we don't have to worry about the query or anything else. ex:
-*/
-if( parse_url( $_SERVER["REQUEST_URI"] )["path"] === "/" ){
-	echo 'welcome home';
-}
-
-/////////////////////
 
 $uri = parse_url( $_SERVER["REQUEST_URI"] )["path"];
 
-if($uri === '/') {
-	require 'controllers/index.php';
-}
-else if ($uri === '/about') {
-	require 'controllers/about.php';
-} else if ($uri === '/contact') {
-	require 'controllers/contact.php';
-} else {
-	echo 'what is wrong';
+// We refactor the code by creating an array that associate every "url" with a corresponding "controller",
+// instead of using the "if else" statement.
+
+$routes = [
+	'/' => 'controllers/index.php',
+	'/about' => 'controllers/about.php',
+	'/contact' => 'controllers/contact.php',
+];
+
+if(array_key_exists($uri, $routes)){
+	// The "array_key_exists()" function takes a key (which is our "$uri" variable) and checks if it exists in an associative
+	// array (which is "$routes")
+	// Now if($uri == '/') then we require 'controllers/index.php' which is $routes[$uri]:
+	require $routes[$uri];
 }
