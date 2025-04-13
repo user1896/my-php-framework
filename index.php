011ee9f1e38759148f5f870cf5b29bc30b145003
user1896 <abde.user15@gmail.com>
2025-04-14 00:05:37 +0200

    Refactor the 'if else' logic that handles the routing into a single function

--- index.php
+++ index.php
@@ -10,19 +10,22 @@ $routes = [
 	'/contact' => 'controllers/contact.php',
 ];
 
-// Refactor "abort()" to handle any response status code:
-function abort($code = 404) { // Set the default value for the parameter as "404" if the user didn't specify anything.
+function abort($code = 404) {
 	http_response_code($code);
 
 	// we should have a safety mechanism here to check if the page "views/{$code}.php" exists
-	// but now we just assume it exists:
 	require "views/{$code}.php";
 
 	die();
 }
 
-if(array_key_exists($uri, $routes)){
-	require $routes[$uri];
-} else {
-	abort();
+function routeToController($uri, $routes){
+	// Refactor this logic that handles the routing, by putting it inside a function:
+	if(array_key_exists($uri, $routes)){
+		require $routes[$uri];
+	} else {
+		abort();
+	}
 }
+
+routeToController($uri, $routes);
