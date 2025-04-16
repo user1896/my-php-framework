44a5549695f87a89933ea620d9312a5e6f8ac777
user1896 <abde.user15@gmail.com>
2025-04-17 00:44:17 +0200

    A note should only be viewed by the user who created it

--- controllers/note.php
+++ controllers/note.php
@@ -6,6 +6,20 @@ $config = require('config.php');
 $db = new Database($config['database'], 'root', 'lalaseadel44');
 
 $query = "select * from notes where id = :id";
-$note = $db->query($query, ['id' => $_GET['id']])->fetch();
+
+$note = $db->query($query, [
+	'id' => $_GET['id']
+])->fetch();
+
+// if $query is false that means the note doesn't exist.
+// Here we don't show a PHP warning in our website, instead we load the 404 page.
+if(!$note) {
+	abort();
+}
+
+// If the current user in not the one who wrote the note then he should not be able to access it.
+if($note['user_id'] != 1){ // let's suppose that user "1" is the current user (until we implement authentication).
+	abort(403); // the http status code "403" is called "forbiden" it is used when the user is not authorise to access data.
+}
 
 require "views/note.view.php";
--- views/partials/main.php
+++ views/partials/main.php
@@ -1,4 +1,3 @@
-<!-- add the class="highlight-txt" to h1 only when we're in the home page  -->
 <h1 <?php if($uri == '/') echo ' class="highlight-txt" ' ?> >
 	<?= $title ?> page
 </h1>
