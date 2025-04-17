5af242872b28d254cb8e9d574d87d6577744fe52
user1896 <abde.user15@gmail.com>
2025-04-17 23:00:26 +0200

    Separate the routes from the router

--- router.php
+++ router.php
@@ -1,14 +1,6 @@
 <?php
 
-$uri = parse_url( $_SERVER["REQUEST_URI"] )["path"];
-
-$routes = [
-	'/' => 'controllers/index.php',
-	'/about' => 'controllers/about.php',
-	'/contact' => 'controllers/contact.php',
-	'/notes' => 'controllers/notes.php',
-	'/note' => 'controllers/note.php',
-];
+$routes = require 'routes.php';
 
 function abort($code = 404) {
 	http_response_code($code);
@@ -26,5 +18,7 @@ function routeToController($uri, $routes){
 		abort();
 	}
 }
+
+$uri = parse_url( $_SERVER["REQUEST_URI"] )["path"];
 
 routeToController($uri, $routes);
--- views/notes.view.php
+++ views/notes.view.php
@@ -2,11 +2,16 @@
 <?php require "partials/nav.php" ?>
 <?php require "partials/main.php" ?>
 
-<?php foreach($notes as $note) : ?>
-	<a href="/note?id=<?= $note['id'] ?>" class="notesAnchor">
-		<li><?= $note['body'] ?></li>
-	</a>
-<?php endforeach; ?>
+<ul>
+	<?php foreach($notes as $note) : ?>
+		<a href="/note?id=<?= $note['id'] ?>" class="notesAnchor">
+			<li><?= $note['body'] ?></li>
+		</a>
+	<?php endforeach; ?>
+</ul>
+<p>
+	<a href="#">Create Note</a>
+</p>
 
 </body>
 </html>
