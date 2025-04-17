aa90d4b20c0fd40cbcf06c7881374f90978050dd
user1896 <abde.user15@gmail.com>
2025-04-17 20:30:53 +0200

    create a fetch method (call it find()) that contains PDO's fetch method inside so we can add more functionality to it

--- Database.php
+++ Database.php
@@ -2,6 +2,7 @@
 
 class Database {
 	private $connection;
+	private $statement;
 
 	public function __construct($config, $username = 'root', $password = '') {
 		$dsn = 'mysql:'.http_build_query($config,'',';');
@@ -9,11 +10,31 @@ class Database {
 			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 		]);
 	}
+
 	public function query($query, $params = []) {
-		$statement = $this->connection->prepare($query);
+		$this->statement = $this->connection->prepare($query);
 
-		$statement->execute($params);
+		$this->statement->execute($params);
 
-		return $statement;
+		return $this;
+	}
+
+	// We wrap PHP PDO's fetch() method inside our own fetch method(we call it find())
+	// This way we'll have control over fetch() and we can add more functionality to it.
+	// For example if it didn't fetch anything and the result is false, make it abort() to the 404 page.
+	// This is better than writting this logic outside of fetch() while it always needs it.
+	public function find() { // My own fetch method
+		return $this->statement->fetch(); // that calls PHP PDO's fetch() method.
+	}
+
+	public function findOrFail() {
+		$result = $this->find();
+
+		// if $query is false that means the result doesn't exist.
+		if( !$result ) {
+			abort();
+		}
+
+		return $result;
 	}
 }
--- controllers/note.php
+++ controllers/note.php
@@ -9,22 +9,9 @@ $query = "select * from notes where id = :id";
 
 $note = $db->query($query, [
 	'id' => $_GET['id']
-])->fetch();
-
-// if $query is false that means the note doesn't exist.
-if(!$note) {
-	abort();
-}
-
-// A special number is a number that has significance and special meaning that is not explicitly declared.
-// Below we have two special numbers: "1" and "403".
-// Now imagive 6 months from now you come back to this code and you forget what those numbers mean.
-// One solution is to extract them into variables.
+])->findOrFail();
 
 $currentUserId = 1;
-
-// When it comes to status codes (like 403) its better to create a new file (class) that handles all of them.
-// We call it Response.php
 
 // Only the ownner of the note can access it.
 if($note['user_id'] != $currentUserId){
