ff4464fb4e73f7a6bcbfcca5255c40edacbf3b2b
user1896 <abde.user15@gmail.com>
2025-04-18 20:50:49 +0200

    Request methods and my first form

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -2,4 +2,9 @@
 
 $title = 'Create a new note';
 
+// We didn't specify the action method, so by default the same page will be the destination for the POST request.
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+	mydebug($_POST);
+}
+
 require "views/note-create.view.php";
