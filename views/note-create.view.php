e3b42d3f9ced914ca05544f5293244b77dc9b9f7
user1896 <abde.user15@gmail.com>
2025-04-19 00:56:07 +0200

    Give the user feedback about the errors that accured while trying to post into the database

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -6,9 +6,18 @@
 	<div>
 		<label for="body">note body</label>
 	</div>
+
 	<div>
 		<textarea name="body" id="body" rows="5" cols="40" placeholder="Here's an idea for a note ..." required></textarea>
 	</div>
+
+	<!-- Give the user feedback about the errors that accured while trying to post into the database -->
+	<div>
+		<?php if(isset($errors['body'])) : ?> <!-- if the error exists -->
+			<p class="highlight-txt"><?= $errors['body'] ?></p> <!-- then display the error -->
+		<?php endif ?>
+	</div>
+	
 	<button type="submit">Create</button>
 </form>
 
