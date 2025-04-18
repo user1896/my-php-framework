364ba740bbebdf0786fa2eb6a7065e6478cca92d
user1896 <abde.user15@gmail.com>
2025-04-19 00:41:41 +0200

    prevent the user from posting an empty body

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -5,11 +5,24 @@ $title = 'Create a new note';
 $config = require('config.php');
 $db = new Database($config['database'], 'root', 'lalaseadel44');
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-	$db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-		'body' => $_POST['body'],
-		'user_id' => 1
-	]);
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// An array called $errors that hold all our errors.
+	$errors = [];
+
+	// did the user entered an empty string
+	if(strlen($_POST['body']) == 0) {
+		$errors['body'] = 'A body is required';
+	}
+
+	// If there are no validation errors:
+	if( empty($errors) ) {
+		// then it is safe to post into the database:
+		$db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+			'body' => $_POST['body'],
+			'user_id' => 1
+		]);
+	}
+
 }
 
 require "views/note-create.view.php";
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -7,7 +7,7 @@
 		<label for="body">note body</label>
 	</div>
 	<div>
-		<textarea name="body" id="body" rows="5" cols="40" placeholder="Here's an idea for a note ..."></textarea>
+		<textarea name="body" id="body" rows="5" cols="40" placeholder="Here's an idea for a note ..." required></textarea>
 	</div>
 	<button type="submit">Create</button>
 </form>
