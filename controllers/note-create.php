468e2807760708cc275d6d65c4e741c75a7c9c6e
user1896 <abde.user15@gmail.com>
2025-04-18 23:08:38 +0200

    POST into the database

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -2,9 +2,24 @@
 
 $title = 'Create a new note';
 
-// We didn't specify the action method, so by default the same page will be the destination for the POST request.
+$config = require('config.php');
+$db = new Database($config['database'], 'root', 'lalaseadel44');
+
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-	mydebug($_POST);
+	$db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+		'body' => $_POST['body'],
+		'user_id' => 1
+	]);
 }
+// We should not let the user have full control of what they can post into the database.
+// Instead of writting a note, what if they deside to enter something like this:
+// <script>alert('my note is an alert, haha')</script>
+// then everytime other users load the page to see other peoples' notes they will get this "alert".
+// a js script can be much more sinister, or a css class that breaks the website layout.
+// a user should definatly not get this level of power.
+
+// There are two solutions to this:
+// The First is to sanitize the body of the note before it enters the database.
+// The Second is to allow it to enter the database but then always escape it when displaying it.
 
 require "views/note-create.view.php";
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -2,19 +2,6 @@
 <?php require "partials/nav.php" ?>
 <?php require "partials/main.php" ?>
 
-<!--
-By default a "form" will submit using a GET request. We can see that every input's name value is sent through the url.
-Even when we just click a link to go to a url, that's a GET request, because it's through the url.
-If we don't want the data to be a part of the url query string we have to use POST request, we need to specify that
-explicitly in the form's method attribute.
-
-The POST request by default will send the data to the same page it is used in if we don't specify a destination 
-in the attribute "action", so it will rereload the page, and now $_POST will have the data sent.
-
-We can see what request we're getting, through the global variable "$_SERVER":
-$_SERVER['REQUEST_METHOD']
--->
-
 <form method="POST" action="">
 	<div>
 		<label for="body">note body</label>
