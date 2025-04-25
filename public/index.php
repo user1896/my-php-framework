bff249fc8ea7ee6e457c0f90696dfa146c32a7be
user1896 <abde.user15@gmail.com>
2025-04-25 23:54:10 +0200

    automatically load classes when we instanciate them

--- public/index.php
+++ public/index.php
@@ -3,6 +3,20 @@
 const BASE_PATH = __DIR__ . '/../'; // BASE_PATH will point to an absolute path to the root of the project.
 
 require BASE_PATH . 'functions.php';
-require base_path('Database.php');
-require base_path('Response.php');
+
+// Everytime I need access to a new class I have to require it withing public/index.php, it will be better
+// if I auto load these classes when I instantiate them
+
+// automatically load classes when we instanciate them:
+
+// The function 'spl_autoload_register()' triggers whenever we instantiate a class.
+// We can use it to describes how we should auto load classes that are not immediately available.
+// It accepts a callback function as an argument, and this cb function takes a className as an argument, then we
+// describe what we should do inside the body of the cb function when the class is instantiated.
+spl_autoload_register( function($className) {
+	require base_path($className . '.php');
+	// (for example when we instantiate the class "Database" we want to require the file "Database.php")
+	// and this is true for all of our classes, we declare them in a separate file then we instantiate them elsewhere.
+} );
+
 require base_path('router.php');
