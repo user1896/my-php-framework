7a05c8eba62776d3c02884599a4ccfb3bf8a40c3
user1896 <abde.user15@gmail.com>
2025-04-26 00:12:56 +0200

    Create the 'Core' folder that will contain all our generic classes

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,6 +1,6 @@
 <?php
 
-require base_path('Validator.php');
+require base_path('Core/Validator.php');
 
 $config = require base_path('config.php');
 
--- public/index.php
+++ public/index.php
@@ -4,19 +4,13 @@ const BASE_PATH = __DIR__ . '/../'; // BASE_PATH will point to an absolute path 
 
 require BASE_PATH . 'functions.php';
 
-// Everytime I need access to a new class I have to require it withing public/index.php, it will be better
-// if I auto load these classes when I instantiate them
-
 // automatically load classes when we instanciate them:
-
 // The function 'spl_autoload_register()' triggers whenever we instantiate a class.
-// We can use it to describes how we should auto load classes that are not immediately available.
-// It accepts a callback function as an argument, and this cb function takes a className as an argument, then we
-// describe what we should do inside the body of the cb function when the class is instantiated.
 spl_autoload_register( function($className) {
-	require base_path($className . '.php');
-	// (for example when we instantiate the class "Database" we want to require the file "Database.php")
-	// and this is true for all of our classes, we declare them in a separate file then we instantiate them elsewhere.
+	require base_path('Core/' . $className . '.php');
 } );
 
 require base_path('router.php');
+
+// We have classes like "Database", "Response", and "Validator" that are not uniquely related to the app we're
+// building, they are generic, so it will be better if we separate them into there own file, we call it "Core".
