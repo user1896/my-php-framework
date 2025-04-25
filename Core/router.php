c911bf814453a91b9f02c8ef9756c0ed1155c3a3
user1896 <abde.user15@gmail.com>
2025-04-26 00:32:25 +0200

    add the file 'functions.php' and 'router.php' to our Core folder and update the paths

--- Core/router.php
+++ Core/router.php
@@ -1,19 +1,20 @@
 <?php
 
-$routes = require 'routes.php';
+// $routes = require 'routes.php';
+$routes = require base_path('routes.php');
 
 function abort($code = 404) {
 	http_response_code($code);
 
 	// we should have a safety mechanism here to check if the page "views/{$code}.php" exists
-	require "views/{$code}.php";
+	require base_path("views/{$code}.php");
 
 	die();
 }
 
 function routeToController($uri, $routes){
 	if(array_key_exists($uri, $routes)){
-		require $routes[$uri];
+		require base_path($routes[$uri]);
 	} else {
 		abort();
 	}
--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 const BASE_PATH = __DIR__ . '/../'; // BASE_PATH will point to an absolute path to the root of the project.
 
-require BASE_PATH . 'functions.php';
+require BASE_PATH . 'Core/functions.php';
 
 // automatically load classes when we instanciate them:
 // The function 'spl_autoload_register()' triggers whenever we instantiate a class.
@@ -10,7 +10,4 @@ spl_autoload_register( function($className) {
 	require base_path('Core/' . $className . '.php');
 } );
 
-require base_path('router.php');
-
-// We have classes like "Database", "Response", and "Validator" that are not uniquely related to the app we're
-// building, they are generic, so it will be better if we separate them into there own file, we call it "Core".
+require base_path('Core/router.php');
