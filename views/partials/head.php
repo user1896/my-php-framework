2074767d3ae78c1bf2c681fd0b55a554733c52dd
user1896 <abde.user15@gmail.com>
2025-05-03 08:45:34 +0200

    fixing the css file

--- views/partials/head.php
+++ views/partials/head.php
@@ -4,7 +4,6 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?= $title ?></title>
-	<!-- <link rel="stylesheet" href="style.css"> -->
-	<link rel="stylesheet" href="<?= base_path("style.css") ?>" >
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
