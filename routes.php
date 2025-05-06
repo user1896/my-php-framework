e08f6868726a215d700867971dc9733007e58726
user1896 <abde.user15@gmail.com>
2025-05-06 21:43:45 +0200

    adding our first middleware

--- routes.php
+++ routes.php
@@ -4,7 +4,8 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+// make only authenticated user able to access the notes
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 
@@ -17,5 +18,10 @@ $router->post('/notes', 'controllers/notes/store.php');
 // and the controller is 'store.php'.
 // the route '/notes-create' only handle the view of creating a note.
 
-$router->get('/register', 'controllers/registration/create.php');
+// make the get('/register') route restricted to only guests
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
+
+// If we're already signed in we need some kined of protection that restricts us from accessing certin pages,
+// like we can't go the registration page again, we're already signed in
+// Also if we're not signed in we can't create a new note.
