05059af51ea9e9171007c86b23f7f2a4ca9d6e28
user1896 <abde.user15@gmail.com>
2025-05-07 23:20:30 +0200

    Refactor error handling in the login page

--- controllers/sessions/store.php
+++ controllers/sessions/store.php
@@ -37,34 +37,30 @@ $user = $db->query('select * from users where email = :email', [
 	'email' => $email
 ])->find();
 
-// If no user is found, then we reload the login view, and send an error.
-if(!$user) {
-	return view('sessions/create.view.php', [
-		'title' => 'Login',
-		'errors' => [
-			'email' => 'No matching account found for that email address.'
-		],
-	]);
+// If a user is found: then we reload the login view, and send an error.
+if($user) {
+	// We check if the password provided matches what we have in the database:
+	// our password in the database is hashed, so we use the function password_verify() provided by PHP to verify
+	// if a hashed password matches what we have.
+	if(password_verify($password, $user['password'])){
+		// If the password is correct, then log them in using sessions.
+		login([
+			'email' => $email
+		]);
+	
+		// then redirect them to the home page.
+		header('location: /');
+		exit();
+	}
 }
 
-// at this point, we have a user, but we don't know if the password provided matches what we have in the database:
-// our password in the database is hashed, so we use the function password_verify() provided by PHP to verify
-// if a hashed password matches what we have.
-if(password_verify($password, $user['password'])){
-	// If the password is correct, then log them in using sessions.
-	login([
-		'email' => $email
-	]);
 
-	// then redirect them to the home page.
-	header('location: /');
-	exit();
-}
-
-// If we reach this point, it means the password validation failed, so reload the login view, and send an error.
+// If we reach this point, it means one of two things:
+// either We didn't find a user, or the password validation failed
+// In both cases we reload the login view, and send an error.
 return view('sessions/create.view.php', [
 	'title' => 'Login',
 	'errors' => [
-		'password' => 'you forget your password silly booooy'
+		'password' => 'No matching account found for that email address and password'
 	],
 ]);
